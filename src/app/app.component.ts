e8c5bd16921afd5cab3468e7131a4f243a9c83e4
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2024-09-03 12:38:37 -0600

    create first draft for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }
+  ]
 })
 export class AppComponent {
   title = 'drivers-web';
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { constants } from '../../constants/constants';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+
+  formLogin = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern(constants.PATTERN_EMAIL)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  constructor() { }
+
+  login() {
+    throw new Error('Method not implemented.');
+  }
 }
