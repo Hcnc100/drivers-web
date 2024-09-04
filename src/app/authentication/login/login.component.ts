d4d7c6f3bb5f37ebd6010d1172afd5d76b8f4c63
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2024-09-03 21:30:33 -0600

    feat(LoginComponent): :sparkles: Create basic logic for login user

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,6 +6,10 @@ import { constants } from '../../constants/constants';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../services/auth.service';
 import { LoginDTO } from '../model/LoginDTO';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { finalize, merge, tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login',
@@ -14,46 +18,114 @@ import { LoginDTO } from '../model/LoginDTO';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-
   formLogin = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.pattern(constants.PATTERN_EMAIL)]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    password: new FormControl('', [Validators.required])
   });
+
+  private _isLoading = signal<boolean>(false);
+  isLoading = this._isLoading.asReadonly();
+
+  private _isPasswordVisible = signal<boolean>(false);
+  isPasswordVisible = this._isPasswordVisible.asReadonly();
+
+  private _errorEmail = signal<string>('');
+  errorEmail = this._errorEmail.asReadonly();
+
+  private _errorPassword = signal<string>('');
+  errorPassword = this._errorPassword.asReadonly();
+
+  private errors = {
+    email: [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Invalid email' }
+    ],
+    password: [
+      { type: 'required', message: 'Password is required' }
+    ]
+  }
 
   constructor(
     private authService: AuthService
-  ) { }
+  ) {
+
+    this.createListenerByFormControl(this.formLogin.controls.email, () => {
+      this.validateControl(this.formLogin.controls.email, this.errors.email, this._errorEmail);
+    });
+    this.createListenerByFormControl(this.formLogin.controls.password, () => {
+      this.validateControl(this.formLogin.controls.password, this.errors.password, this._errorPassword);
+    });
+  }
+  private validateControl(control: FormControl, errors: { type: string, message: string }[], signalError: WritableSignal<string>) {
+    if (control.invalid) {
+      const error = errors.find(error => control.hasError(error.type));
+      if (error) {
+        signalError.set(error.message);
+      }
+    } else {
+      signalError.set('');
+    }
+  }
+
+  private createListenerByFormControl(control: FormControl, updateErrorMessage: () => void) {
+    merge(control.statusChanges, control.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => updateErrorMessage());
+  }
 
   login() {
     if (this.formLogin.valid) {
       const loginDTO = this.generateLoginForm();
-      this.authService.login(loginDTO).subscribe({
-        next: (response: any) => {
-          console.log(response);
-        },
-        error: (error: any) => {
-          console.error(error);
-        }
-      });
+      this.loginWithCredential(loginDTO)
     } else {
       console.log('Formulário inválido');
     }
   }
 
+  private loginWithCredential(loginDTO: LoginDTO) {
+    this._isLoading.set(true);
+    this.formLogin.disable();
+    this.authService.login(loginDTO)
+      .subscribe({
+        next: (response: any) => {
+          console.log(response);
+        },
+        error: (error: any) => {
+          console.error(error);
+        },
+      })
+      .add(() => {
+        this._isLoading.set(false);
+        this.formLogin.enable();
+      });
+  }
+
   private generateLoginForm(): LoginDTO {
-    const email = this.formLogin?.get('email')?.value!;
-    const password = this.formLogin.get('password')?.value!;
+    const email = this.formLogin.controls.email.value!;
+    const password = this.formLogin.controls.password.value!;
 
     return {
       email: email,
       password: password
     };
   }
+
+  togglePassword(event: MouseEvent) {
+    this._isPasswordVisible.update(value => !value);
+    console.log('togglePassword', this.isPasswordVisible());
+    event.stopPropagation();
+  }
+
+
 }
+
+
