dd63fb1e99f684fdde712d9409f3331799bd6a8c
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2024-09-10 21:18:34 -0600

    feat(Pagination): :lipstick: Finish basic search

--- src/app/shared/pagination/components/search-grid/search-grid.component.ts
+++ src/app/shared/pagination/components/search-grid/search-grid.component.ts
@@ -3,18 +3,24 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-search-grid',
   standalone: true,
   imports: [
-    FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, ReactiveFormsModule,
+    FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, ReactiveFormsModule, MatInputModule, MatButtonModule
   ],
   templateUrl: './search-grid.component.html',
   styleUrl: './search-grid.component.css'
 })
 export class SearchGridComponent {
 
+
   search = model('');
+
+  clearSearch() {
+    this.search.set('');
+  }
 } 
