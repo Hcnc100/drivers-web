f95179da29a52ddabdc3918a08428fc7194762bb
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2025-01-05 12:22:49 -0600

    se agrego el test del search grid

--- src/app/shared/pagination/components/search-grid/search-grid.component.spec.ts
+++ src/app/shared/pagination/components/search-grid/search-grid.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 import { SearchGridComponent } from './search-grid.component';
@@ -13,8 +13,7 @@ describe('SearchGridComponent', () => {
       providers: [
         provideNoopAnimations()
       ]
-    })
-      .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SearchGridComponent);
     component = fixture.componentInstance;
@@ -23,5 +22,39 @@ describe('SearchGridComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set search when updateSearch is called', () => {
+    spyOn(component['searchSubject'], 'next');
+    const event = { target: { value: 'test' } } as unknown as Event;
+
+    component.updateSearch(event);
+
+    expect(component['searchSubject'].next).toHaveBeenCalledWith('test');
+  });
+
+  it('should set search to an empty string when clearSearch is called', () => {
+    spyOn(component.search, 'set');
+
+    component.clearSearch();
+
+    expect(component.search.set).toHaveBeenCalledWith('');
+  });
+
+  it('should call search.set after debounce time in ngOnInit', fakeAsync(() => {
+    spyOn(component.search, 'set');
+
+    component['searchSubject'].next('debounced test');
+    tick(1000);
+
+    expect(component.search.set).toHaveBeenCalledWith('debounced test');
+  }));
+
+  it('should unsubscribe from searchSubject on ngOnDestroy', () => {
+    spyOn(component['searchSubject'], 'unsubscribe');
+
+    component.ngOnDestroy();
+
+    expect(component['searchSubject'].unsubscribe).toHaveBeenCalled();
   });
 });
