b3495c155652e7c19bae7b9be22ad1ceea39521c
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2025-01-18 15:54:16 -0600

    refactor configuración de Karma para mejorar la cobertura y agregar soporte para Jasmine y ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,26 @@
-module.exports = {
-  coverageIstanbulReporter: {
-    thresholds: {
-      global: {
-        statements: 80,
-        lines: 80,
-        branches: 70,
-        functions: 80
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
       }
-    }
-  }
+    },
+    reporters: ['progress', 'coverage-istanbul'],
+    browsers: ['ChromeHeadless'],
+    singleRun: true
+  });
 };
