c64aaff3507a06cb3f5703a12f78b04e1d362c5c
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2025-02-06 23:00:57 -0600

    test: add tests for navigation and password reset email functionality in authentication components

--- src/app/authentication/send-reset-password/send-reset-password.component.spec.ts
+++ src/app/authentication/send-reset-password/send-reset-password.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SendResetPasswordComponent } from './send-reset-password.component';
+import { AuthService } from '../services/auth.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { of, throwError } from 'rxjs';
 
 describe('SendResetPasswordComponent', () => {
   let component: SendResetPasswordComponent;
   let fixture: ComponentFixture<SendResetPasswordComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    authService = jasmine.createSpyObj('AuthService', ['sendResetPassword']);
     await TestBed.configureTestingModule({
-      imports: [SendResetPasswordComponent]
+      imports: [SendResetPasswordComponent, NoopAnimationsModule],
+      providers: [{ provide: AuthService, useValue: authService }]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SendResetPasswordComponent);
     component = fixture.componentInstance;
@@ -19,5 +25,35 @@ describe('SendResetPasswordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should not send reset password when form is invalid', () => {
+    component.sendEmail();
+    expect(authService.sendResetPassword).not.toHaveBeenCalled();
+  });
+
+
+  it('should send reset password when form is valid', () => {
+
+    authService.sendResetPassword.and.returnValue(of({
+      message: 'Email sent'
+    }));
+
+    component.form.patchValue({ email: 'example@email.com' });
+    component.sendEmail();
+    expect(authService.sendResetPassword).toHaveBeenCalled();
+    expect(component.isLoading()).toBeFalse();
+  });
+
+  it('should show error message when send reset password fails', () => {
+    authService.sendResetPassword.and.returnValue(throwError(() => new Error('Error')));
+    component.form.patchValue({ email: 'example@email.com' });
+
+    component.sendEmail();
+    expect(authService.sendResetPassword).toHaveBeenCalled();
+
+    expect(component.isLoading()).toBeFalse();
+
   });
 });
